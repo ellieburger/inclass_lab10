1a56119ad07524b57f9ed5d33af459ee032d74ab
ellieburger <lee.liz2002ilovenh1d520@gmail.com>
2023-12-15 14:06:41 +0800

    good

--- app/src/main/java/com/example/myapplication/OnMapReadyCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.maps.GoogleMap;
-
-public interface OnMapReadyCallback {
-    void onMapReady(@NonNull GoogleMap googleMap);
-}
